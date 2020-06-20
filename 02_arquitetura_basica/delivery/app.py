0681c8df45d15fdf913ce951b5341ec89811b227
bernarducs <bernarducs@gmail.com>
2020-06-20 15:27:03 -0300

    02 app factory (evitando circular imports)

---
+++ 02_arquitetura_basica/delivery/app.py
@@ -0,0 +1,13 @@
+"""
+estrutura básica do flask
+"""
+import views
+from flask import Flask
+
+
+
+def create_app():
+    """factory principal"""
+    app = Flask(__name__)
+    views.init_app(app)
+    return app
